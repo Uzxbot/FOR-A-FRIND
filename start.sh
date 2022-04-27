406d250ea97e67736e35d525bbdee7b0f9205298
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-27 08:37:47 +0530

    update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/illuzX/AtwFilt /app
+  git clonehttps://github.com/Uzxbot/FOR-A-FRIND /app
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /app
