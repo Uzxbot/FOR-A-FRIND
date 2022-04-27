cc49258abc937d02143f382039041772784de51c
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-27 08:36:40 +0530

    update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/illuzX/AtwFilt /app
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /app
+fi
+cd /app
+pip3 install -U -r requirements.txt
 echo "starting UrlUploader ~@Tellybots";
 python3 -m bot
