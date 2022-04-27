6685f0a1f7e70bef854d0eeed790b3b67c9bcb41
illuzX <98640218+illuzX@users.noreply.github.com>
2022-04-27 08:39:29 +0530

    update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clonehttps://github.com/Uzxbot/FOR-A-FRIND /app
+  git clone https://github.com/Uzxbot/FOR-A-FRIND /app
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /app
 fi
 cd /app
 pip3 install -U -r requirements.txt
-echo "starting UrlUploader ~@Tellybots";
+echo "starting UrlUploader";
 python3 -m bot
